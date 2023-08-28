7ea4f73af1d47c322f6dab91368696187b7f9c6b
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-28 12:40:56 +0200

    projectSelenium1

---
+++ src/test/java/com/alexandr/test/day1_selenium_intro/SeleniumTest.java
@@ -0,0 +1,25 @@
+package com.alexandr.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+
+        //1- set up the web driver
+        WebDriverManager.chromedriver().setup();
+
+        //2- create the instance of the chrome driver
+        WebDriver driver = new ChromeDriver();
+
+        //3- test if driver is working
+//        driver.get("https://www.facebook.com");
+        driver.get("https://www.google.com");
+//        driver.findElement(By.xpath("(//button[@value='1'])[2]")).click();
+        driver.findElement(By.xpath("//*[.='Alles accepteren']")).click();
+
+    }
+}
