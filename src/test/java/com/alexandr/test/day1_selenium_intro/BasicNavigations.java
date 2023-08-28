1b287ad0bf1507cde8eda2abd2f5ea97a51185de
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-28 15:53:55 +0200

    projectSelenium1

---
+++ src/test/java/com/alexandr/test/day1_selenium_intro/BasicNavigations.java
@@ -0,0 +1,12 @@
+package com.alexandr.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasicNavigations {
+
+    public static void main(String[] args) {
+
+        //1- setup the browser driver
+        WebDriverManager.chromedriver().setup();
+    }
+}
