948cab06074fa13cc6e635629b9c72fefedbb1fd
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-30 17:06:46 +0200

    day 2 practice 3 click to link

---
+++ src/test/java/com/alexandr/test/day2_locators_getText_getAttribute/P4_GoogleSearchVerification.java
@@ -0,0 +1,27 @@
+package com.alexandr.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P4_GoogleSearchVerification {
+
+    public static void main(String[] args) {
+
+        //TC #4:	Google	search
+        //1- Open	a	chrome	browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2- Go	to:	https://google.com
+        driver.get("https://google.com");
+
+        //3- Write	“apple”	in	search	box
+        //4- Click	google	search	button
+        //5- Verify	title:
+        //Expected:	Title	should	start	with	“apple”	word
+
+
+    }
+}
