31cee65866682a418e64a2b397194d38e5d69719
Dst07032023 <bovari.deonisis@gmail.com>
2023-08-30 22:11:23 +0200

    day 2 practice 6 Cybertek Title verification

---
+++ src/test/java/com/alexandr/test/day2_locators_getText_getAttribute/P6_CybertekTitleVerification.java
@@ -0,0 +1,30 @@
+package com.alexandr.test.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class P6_CybertekTitleVerification {
+
+    public static void main(String[] args) {
+
+        //TC #6: Practice Cybertek - Class locator practice
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to http://practice.cybertekschool.com/inputs
+        driver.get("http://practice.cybertekschool.com/inputs");
+
+        //3. Click "Home" button
+        //1st - locate the web element using class attribute value
+        //2nd - click
+
+        driver.findElement(By.className("nav-link")).click();
+
+        //4. Verify title as expected:
+        //Expected: Practice
+    }
+}
