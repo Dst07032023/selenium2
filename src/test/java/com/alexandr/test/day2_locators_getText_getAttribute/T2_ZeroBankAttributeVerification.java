1909bb8aa695015d60511c4d4ecbcd7ac6a38740
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-01 12:18:49 +0200

    day 2 practice 8 ZeroBank verifications

--- src/test/java/com/alexandr/test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
+++ src/test/java/com/alexandr/test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
@@ -1,7 +1,9 @@
 package com.alexandr.test.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T2_ZeroBankAttributeVerification {
@@ -17,9 +19,35 @@ public class T2_ZeroBankAttributeVerification {
         //2. Go to http://zero.webappsecurity.com/login.html
         driver.get("http://zero.webappsecurity.com/login.html");
 
+        //LOCATE THE LINK AND STORE INSIDE OF WebElement
+
+        WebElement zeroBankLink = driver.findElement(By.className("brand"));
+        //All of the below is locating same web element
+//        WebElement zeroBankLink = driver.findElement(By.ByLinkText("Zero Bank"));
+//        WebElement zeroBankLink = driver.findElement(By.tagName("a"));
+
         //3. Verify link text from top left:
         //Expected: “Zero Bank”
+        String expectedLinkText = "Zero Bank";
+        String actualLinkText = zeroBankLink.getText();
+        //driver.findElement(By.className("brand")).getText();
+
+        if (actualLinkText.equals(expectedLinkText)) {
+            System.out.println("Link text verification PASSED!");
+        }else{
+            System.out.println("Link text verification FAILED!!!");
+        }
         //4. Verify link href attribute value contains:
         //Expected: “index.html”
+        String expectedInHrefValue = "index.html";
+
+        String actualHrefValue = zeroBankLink.getAttribute("href");
+
+        if (actualHrefValue.contains(expectedInHrefValue)) {
+            System.out.println("HREF attribute value verification PASSED!");
+        }else{
+            System.out.println("HREF attribute value verification FAILED!!!");
+
+        }
     }
 }
