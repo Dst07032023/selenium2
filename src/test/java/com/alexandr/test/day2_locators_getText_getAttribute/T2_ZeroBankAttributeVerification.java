5802ab98b237c8c72ffa85df0833f12ae250b316
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-01 11:36:32 +0200

    day 2 practice 8 ZeroBank verifications

---
+++ src/test/java/com/alexandr/test/day2_locators_getText_getAttribute/T2_ZeroBankAttributeVerification.java
@@ -0,0 +1,9 @@
+package com.alexandr.test.day2_locators_getText_getAttribute;
+
+public class T2_ZeroBankAttributeVerification {
+
+    public static void main(String[] args) {
+
+        //
+    }
+}
