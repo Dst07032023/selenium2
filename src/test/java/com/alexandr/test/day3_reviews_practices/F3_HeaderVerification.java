6bcc45f7043d2653cdf4063b49a6794e2975ef66
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-01 19:11:40 +0200

    day 3 Facebook task 3

--- src/test/java/com/alexandr/test/day3_reviews_practices/F3_HeaderVerification.java
+++ src/test/java/com/alexandr/test/day3_reviews_practices/F3_HeaderVerification.java
@@ -1,7 +1,9 @@
 package com.alexandr.test.day3_reviews_practices;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class F3_HeaderVerification {
@@ -17,7 +19,25 @@ public class F3_HeaderVerification {
         //2. Go to https://www.facebook.com
         driver.get("https://www.facebook.com");
 
+        driver.findElement(By.xpath("//*[.='Allow all cookies']")).click();
+
+
         //3. Verify header text is as expected:
+        WebElement header = driver.findElement(By.className("_8eso"));
+
+        //short cut for auto-generating variable type
+        //alt+enter -->WINDOWS
+        //option=ENTER-->MA
+
         //Expected: “Connect with friends and the world around you on Facebook.”
+        String expectedHeader = "Connect with friends and the world around you on Facebook.";
+        String actualHeader = header.getText();
+        //---> create webelement<---->getting the text
+
+        if (actualHeader.equals(expectedHeader)) {
+            System.out.println("Header verification passed!");
+        }else{
+            System.out.println("Header verification failed!");
+        }
     }
 }
