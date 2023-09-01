ad5f00cb33b864342d807ce9f164f42ab2f26da8
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-01 19:14:54 +0200

    day 3 Facebook task 4

---
+++ src/test/java/com/alexandr/test/day3_reviews_practices/F4_AttributeValueVerification.java
@@ -0,0 +1,23 @@
+package com.alexandr.test.day3_reviews_practices;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class F4_AttributeValueVerification {
+
+    public static void main(String[] args) {
+
+        //TC #4: Facebook header verification
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //2. Go to https://www.facebook.com
+        driver.get("https://www.facebook.com");
+
+        //3. Verify “Create a page” link href value contains text:
+        //Expected: “registration_form”
+    }
+}
