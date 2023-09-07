e1cae73e6e81430c9749b8bcf6922edaf9090f39
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-07 10:45:30 +0200

    day 4 cssSelector task2

--- src/test/java/com/alexandr/test/day4_cssSelector_xpath/P1_cssSelector_Cybertek.java
+++ src/test/java/com/alexandr/test/day4_cssSelector_xpath/P1_cssSelector_Cybertek.java
@@ -1,7 +1,9 @@
 package com.alexandr.test.day4_cssSelector_xpath;
 
 import com.alexandr.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class P1_cssSelector_Cybertek {
 
@@ -14,8 +16,12 @@ public class P1_cssSelector_Cybertek {
 
         //2. Go to http://practice.cybertekschool.com/forgot_password
         driver.get("http://practice.cybertekschool.com/forgot_password");
+
         //3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
         //a. “Home” link
+//        WebElement homelink = driver.findElement(By.cssSelector("a.nav-link"));
+        WebElement homelink = driver.findElement(By.cssSelector("a[class='nav-link']"));
+
         //b. “Forgot password” header
         //c. “E-mail” text
         //d. E-mail input box
