9c4753b2087549c851fd8fb9b67fe48a4e2012b6
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-07 10:13:57 +0200

    day 4 cssSelector task2

---
+++ src/test/java/com/alexandr/test/day4_cssSelector_xpath/P1_cssSelector_Cybertek.java
@@ -0,0 +1,26 @@
+package com.alexandr.test.day4_cssSelector_xpath;
+
+import com.alexandr.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class P1_cssSelector_Cybertek {
+
+    public static void main(String[] args) {
+
+        //TC #1: PracticeCybertek.com_ForgotPassword WebElement verification
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2. Go to http://practice.cybertekschool.com/forgot_password
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        //3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
+        //a. “Home” link
+        //b. “Forgot password” header
+        //c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button
+        //f. “Powered by Cybertek School” text
+        //4. Verify all WebElements are displayed.
+    }
+}
