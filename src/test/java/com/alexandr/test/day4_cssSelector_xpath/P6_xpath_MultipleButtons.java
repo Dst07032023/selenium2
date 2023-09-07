6ab9aa736fcc41c0d00ec399fe633f1015054581
Dst07032023 <bovari.deonisis@gmail.com>
2023-09-07 15:07:39 +0200

    day 4 xpath task1

--- src/test/java/com/alexandr/test/day4_cssSelector_xpath/P6_xpath_MultipleButtons.java
+++ src/test/java/com/alexandr/test/day4_cssSelector_xpath/P6_xpath_MultipleButtons.java
@@ -1,4 +1,23 @@
 package com.alexandr.test.day4_cssSelector_xpath;
 
+import com.alexandr.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class P6_xpath_MultipleButtons {
+
+    public static void main(String[] args) {
+
+        //TC #6: XPATH LOCATOR practice
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2. Go to http://practice.cybertekschool.com/multiple_buttons
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+
+        //3. Click on Button 1
+        //4. Verify text displayed is as expected:
+        //Expected: “Clicked on button one!”
+
+    }
 }
