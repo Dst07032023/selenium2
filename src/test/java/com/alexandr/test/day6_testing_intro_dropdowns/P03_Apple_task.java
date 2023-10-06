01775003111a603f8f46c455c458d617345505cd
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-06 15:54:06 +0200

    day 6 apple p03 practice

--- src/test/java/com/alexandr/test/day6_testing_intro_dropdowns/P03_Apple_task.java
+++ src/test/java/com/alexandr/test/day6_testing_intro_dropdowns/P03_Apple_task.java
@@ -1,8 +1,11 @@
 package com.alexandr.test.day6_testing_intro_dropdowns;
 
 import com.alexandr.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class P03_Apple_task {
@@ -17,8 +20,14 @@ public class P03_Apple_task {
 
         //2. Go to https://www.apple.com
         driver.get("https://www.apple.com");
+
         //3. Click to all of the headers one by one
             //a. Mac, iPad, iPhone, Watch, TV, Music, Support
+
+        //Storing 11 web elements including "apple" logo, and search box
+        List<WebElement> headerLinks = driver.findElements(By.xpath("//ul[@class =" +
+                "'globalnav-submenu-trigger-group']//li/a "));
+
         //4. Print out the titles of the each page
         //5. Print out total number of links in each page
         //6. While in each page:
