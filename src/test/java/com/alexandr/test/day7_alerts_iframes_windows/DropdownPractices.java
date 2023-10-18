e01a4c8762f820b9fe5a020067980760c3386961
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-18 15:14:19 +0200

    day 7 - dropdown task 1

--- src/test/java/com/alexandr/test/day7_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/alexandr/test/day7_alerts_iframes_windows/DropdownPractices.java
@@ -3,7 +3,10 @@ package com.alexandr.test.day7_alerts_iframes_windows;
 import com.alexandr.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -11,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
     WebDriver driver;
+//    String str1;
 
     /*
     1.Open Chrome browser
@@ -23,18 +27,61 @@ public class DropdownPractices {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("http://practice.cybertekshool.com/dropdown");
+
+//        str1 = "something";
     }
 
 
     @Test
     public void tc1_simple_dropdown_test(){
-
+//        str1.substring(1,5);
         //3.Verify "Simple dropdown" default selected value is correct
         //Expected: "Please selected an option"
         Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
 
+         WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+
+         String actualTextOfCurrentOption = currentlySelectedOption.getText();
+         String expectedText = "Please select an option";
+
+//        Assert.assertTrue(actualTextOfCurrentOption.equals(expectedText));
+        Assert.assertEquals(actualTextOfCurrentOption, expectedText);
+
+         //Doing everything in one line: get currently selected option, get the text and compare against expected
+//        Assert.assertEquals(simpleDropdown.getFirstSelectedOption().getText(),"Please select an option");
+
         //4.Verify "State selection" default selected value is corrected
         //Expected: "Select a State"
 
+        Select stateDropdown = new Select(driver.findElement(By.xpath("select[@id='state']")));
+//        Select stateDropdown = new Select(driver.findElement(By.id("state")));
+
+        String actualStateDropdownText = stateDropdown.getFirstSelectedOption().getText();
+        String expectedStateDropdownText = "Select a State";
+
+        Assert.assertEquals(actualStateDropdownText, expectedStateDropdownText);
+    }
+
+    @Test
+    public void tc2_state_dropdown_test(){
+
+        //LOCATING STATE DROPDOWN
+        Select stateDropdown = new Select(driver.findElement(By.xpath("select[@id='state']")));
+
+        //3.Select Illinois
+        stateDropdown.selectByVisibleText("Illinois");
+
+        //4.Select Virginia
+
+        //5.Select California
+
+        //6.Verify final selected option is California.
+        //Use all Select options. (visible text, value, index)
+
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        driver.close();
     }
 }
