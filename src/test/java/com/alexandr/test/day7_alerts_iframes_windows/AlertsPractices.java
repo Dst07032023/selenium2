231b4e65ddf22d29d86c5611cbe8664f117adc6f
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-18 20:09:58 +0200

    day 7 - Alerts practices

--- src/test/java/com/alexandr/test/day7_alerts_iframes_windows/AlertsPractices.java
+++ src/test/java/com/alexandr/test/day7_alerts_iframes_windows/AlertsPractices.java
@@ -1,8 +1,12 @@
 package com.alexandr.test.day7_alerts_iframes_windows;
 
 import com.alexandr.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +18,27 @@ public class AlertsPractices {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get("http://practice.cybertekshool.com/dropdown");
+        driver.get("http://practice.cybertekshool.com/javascript_alerts");
+    }
+
+    @Test
+    public void information_alert_test() throws InterruptedException {
+
+        //Locating the button for alert to click
+        WebElement informationAlertButton = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+
+        //3.Click to "Click for JS Alert" button
+        informationAlertButton.click();
+
+        Thread.sleep(2000);
+
+        //Switching driver's focus to alert, so we can handle it
+        Alert alert = driver.switchTo().alert();
+
+        //4.Click to OK button from the alert
+        alert.accept();
+
+        //5.Verify "You successfuly clicked an alert" text is displayed.
+        WebElement resultText = driver.findElement(By.xpath("//p[@id='result']"));
     }
 }
