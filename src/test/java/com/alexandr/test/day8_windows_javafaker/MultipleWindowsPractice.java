8acb529b2ba32b8b38e8ce1f07cbdbb532718c15
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-23 12:30:50 +0200

    day8 practice 1

--- src/test/java/com/alexandr/test/day8_windows_javafaker/MultipleWindowsPractice.java
+++ src/test/java/com/alexandr/test/day8_windows_javafaker/MultipleWindowsPractice.java
@@ -3,6 +3,7 @@ package com.alexandr.test.day8_windows_javafaker;
 import com.alexandr.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,13 +24,25 @@ public class MultipleWindowsPractice {
     @Test
     public void multiple_window_test(){
         ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
+//        driver.findElement(By.xpath("//*[.='Alles accepteren']")).click();
+
+
         ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
+//        driver.findElement(By.xpath("(//button[@class='wt-btn wt-btn--filled wt-mb-xs-0'])")).click();
+
         ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com','_blank');");
+//        driver.findElement(By.xpath("//*[.='Allow all cookies']")).click();
+
 
         for (String each : driver.getWindowHandles()) {
 
-            System.out.println("Title in corrent page: " + driver.switchTo().window(each));
+            driver.switchTo().window(each);
+            System.out.println("Title in current page: " + driver.getTitle());
 
+            if(driver.getTitle().contains("Etsy{")){
+                Assert.assertTrue(driver.getTitle().contains("Etsy"));
+                break;
+        }
 
         }
     }
