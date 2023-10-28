9aa306972328ac5f6370c670d79d5f10ba3fd1eb
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-28 09:13:15 +0200

    day9-ConfigurationReader

---
+++ src/test/java/com/alexandr/utilities/ConfigurationReader.java
@@ -0,0 +1,11 @@
+package com.alexandr.utilities;
+
+import java.util.Properties;
+
+/*
+We will create a re-usable method that will be reading from configuration.reader file
+ */
+public class ConfigurationReader {
+
+    Properties properties = new Properties();
+}
