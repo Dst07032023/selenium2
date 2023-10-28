b37bd8838d43b0eaaa7c75688e60dff42c8e9685
Dst07032023 <bovari.deonisis@gmail.com>
2023-10-28 09:39:06 +0200

    day9-ConfigurationReader

--- src/test/java/com/alexandr/utilities/ConfigurationReader.java
+++ src/test/java/com/alexandr/utilities/ConfigurationReader.java
@@ -1,5 +1,7 @@
 package com.alexandr.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 /*
@@ -7,5 +9,32 @@ We will create a re-usable method that will be reading from configuration.reader
  */
 public class ConfigurationReader {
 
-    Properties properties = new Properties();
-}
+    //#1- Create properties object
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+            //#2- Load the file into FileInputStream
+            FileInputStream file = new FileInputStream("configuration.properties");
+
+            //#3- load properties object with the file (configuration.properties)
+            properties.load(file);
+
+            //close the file
+            file.close();
+
+        } catch (IOException e) {
+
+            System.out.println("File not found in Configuration properties.");
+        }
+    }
+
+
+    //USE THE ABOVE CREATED LOGIC TO CREATE A RE-USABLE STATIC METHOD
+    public static String getProperty(String keyWord) {
+        return properties.getProperty("keyWord");
+    }
+
+
+    }
