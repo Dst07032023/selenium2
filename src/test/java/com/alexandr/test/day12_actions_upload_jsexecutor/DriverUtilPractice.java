90bb12932b2931ba213aae7aa748db425f0da6ab
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-14 16:46:24 +0100

    day12-DriverUtilityPractice

--- src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/DriverUtilPractice.java
+++ src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/DriverUtilPractice.java
@@ -1,5 +1,7 @@
 package com.alexandr.test.day12_actions_upload_jsexecutor;
 
+import com.alexandr.utilities.ConfigurationReader;
+import com.alexandr.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class DriverUtilPractice {
@@ -8,7 +10,9 @@ public class DriverUtilPractice {
     public void simple_google_search_test() {
 
         //1- Go to google.com
-        //1- Search for a value
+        //Driver.getDriver() = driver ---> creates driver instance and ready to use.
+        Driver.getDriver().get(ConfigurationReader.getProperty("googleUrl"));
+        //2- Search for a value
         //3- Verify value is contained in the title
     }
 }
