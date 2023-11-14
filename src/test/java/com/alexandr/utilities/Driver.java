001c9b2efb975bb8f202d8afa838ffff47cd1045
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-14 16:09:57 +0100

    day12-close driver method

--- src/test/java/com/alexandr/utilities/Driver.java
+++ src/test/java/com/alexandr/utilities/Driver.java
@@ -58,4 +58,11 @@ public class Driver {
              */
             return driver;
         }
+
+        public static void closeDriver() {
+            if(driver!=null) {
+                driver.quit();
+                driver=null;
+            }
+        }
 }
