b2f338145073d90592be4ebbcc68730c108e9516
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-15 17:50:48 +0100

    day12-Upload practice

--- src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/ActionsPractices.java
+++ src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/ActionsPractices.java
@@ -3,6 +3,7 @@ package com.alexandr.test.day12_actions_upload_jsexecutor;
 import com.alexandr.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -15,8 +16,20 @@ public class ActionsPractices {
 
         //Locating all of the images here:
         WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2= Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+        //Locating all the "user" texts
+        WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
+        WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
+        WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
+        //We must create object of Actions class to be able to use methods coming from there
+        Actions actions = new Actions(Driver.getDriver());
+
 
         //2. Hover	over	to	first	image
+
         //3. Assert:
         //a. “name:	user1”	is	displayed
         //4. Hover	over	to	second	image
