46b0ef62f1062c5410ce36e2cf6395dc4b20f9cf
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-15 17:07:07 +0100

    day12-Singleton practice

--- src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/UploadPractice.java
+++ src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/UploadPractice.java
@@ -1,6 +1,36 @@
 package com.alexandr.test.day12_actions_upload_jsexecutor;
 
+import com.alexandr.utilities.BrowserUtils;
+import com.alexandr.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class UploadPractice {
 
+    @Test
+    public void upload_test() {
 
+        //1. Go to http://practice.cybertekschool.com/upload
+        Driver.getDriver().get("http://practice.cybertekschool.com/upload");
+
+        //2. Find some small file from your computer, and get the path of it.
+        String path = "/Users/cybertekchicago-1/Downloads/lot.jpg";
+
+        //3. Upload the file.
+        WebElement chooseFile = Driver.getDriver().findElement(By.id("file-upload"));
+
+        WebElement uploadButton = Driver.getDriver().findElement(By.id("file-submit"));
+
+        BrowserUtils.sleep(3);
+
+        //Sending the file path to the chooseFile WebElement
+        chooseFile.sendKeys(path);
+
+        //click to upload button
+        uploadButton.click();
+
+        //4. Assert:
+        //-File uploaded text is displayed on the page
+    }
 }
