5daf62131d79042185dd3461530bf46c44642792
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-15 16:29:51 +0100

    day12-Singleton practice

--- src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/Singleton.java
+++ src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/Singleton.java
@@ -14,4 +14,18 @@ public class Singleton {
     // we will ensure we create access only through our getter method
     private static String word;
 
+    //We allow user to access to "word in the way we want them to have
+    public static String getWord() {
+
+        if(word==null){
+            System.out.println("First time call. Word object is null." +
+                    "Assigning value to it now");
+            word = "something";
+        }else{
+            System.out.println("Word already has a value.");
+        }
+
+        return word;
+
+    }
 }
---
+++ src/test/java/com/alexandr/test/day12_actions_upload_jsexecutor/UploadPractice.java
@@ -0,0 +1,6 @@
+package com.alexandr.test.day12_actions_upload_jsexecutor;
+
+public class UploadPractice {
+
+
+}
