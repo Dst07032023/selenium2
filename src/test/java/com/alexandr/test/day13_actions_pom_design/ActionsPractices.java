b9b0cdec946e82bce3211f443c5094eab90b2fee
Dst07032023 <bovari.deonisis@gmail.com>
2023-11-27 15:31:46 +0100

    day13 - ActionsPractices

---
+++ src/test/java/com/alexandr/test/day13_actions_pom_design/ActionsPractices.java
@@ -0,0 +1,14 @@
+package com.alexandr.test.day13_actions_pom_design;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void p2_double_click_test() {
+        //1. Go to https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblcl
+        //2. Switch to iframe.
+        //3. Double click on the text "Double-click me to change my text color."
+        //4. Assert: Text's "style" attribute value contains "red".
+    }
+}
