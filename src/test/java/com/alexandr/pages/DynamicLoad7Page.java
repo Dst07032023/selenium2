579a993e4e9c76a275bef6fc5796d3124c24ec17
Dst07032023 <bovari.deonisis@gmail.com>
2023-12-01 15:56:40 +0100

    day14 - webdriver wait practice

--- src/test/java/com/alexandr/pages/DynamicLoad7Page.java
+++ src/test/java/com/alexandr/pages/DynamicLoad7Page.java
@@ -1,6 +1,8 @@
 package com.alexandr.pages;
 
 import com.alexandr.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad7Page {
@@ -8,4 +10,10 @@ public class DynamicLoad7Page {
     public DynamicLoad7Page(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//strong[.='Done!']")
+    public WebElement doneMessage;
+
+    @FindBy(xpath = "//img[@alt='square pents']")
+    public WebElement spongeBobImage;
 }
--- src/test/java/com/alexandr/test/day14_pom_synchronization/WebDriverWaitPractice.java
+++ src/test/java/com/alexandr/test/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -1,5 +1,7 @@
 package com.alexandr.test.day14_pom_synchronization;
 
+import com.alexandr.utilities.Driver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class WebDriverWaitPractice {
@@ -8,7 +10,12 @@ public class WebDriverWaitPractice {
     public void dynamic_loading_7_test() {
 
         //1.Go to http://practice.cybertekschool.com/dynamic_loading/7
+        Driver.getDriver().get("http://practice.cybertekschool.com/dynamic_loading/7");
+
         //2.Wait until title is "Dynamic title"
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //use the object
         //3.Assert: Message "Done" is displayed.
         //4.Assert: Image is displayed.
     }
